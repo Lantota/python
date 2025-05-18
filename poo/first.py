b7067aa77e343fcb690740d18a2e2e710babc0b1
Tahiriniaina <150047046+Lantota@users.noreply.github.com>
2025-05-18 17:36:33 +0300

    We are inside an Poo repo then all inside are all about Poo

--- poo/first.py
+++ poo/first.py
@@ -1,1 +1,8 @@
-print("Hello wold ! ")
+#We are inside an python file , more precisely inside an POO project
+class name :
+  def __ini__(self):
+    self.name = "Lanto"
+
+ob1 = name()
+name = ob1.name
+print(name)
